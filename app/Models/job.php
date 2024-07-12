dd2eecdfaa2e331d88c66219d659991457dab493
Anuradha_pps <anuradha.pps2024@gmail.com>
2024-07-12 12:07:34 +0530

    done

--- app/Models/job.php
+++ app/Models/job.php
@@ -4,8 +4,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class job extends Model {
+    use HasFactory;
     protected $table = 'job_listing';
     protected $fillable = ['Title','Salary'];
 }
